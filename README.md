65b9772f353067344ff20d3c0681e811847659b8
changhyeon363 <changhyeon363@gmail.com>
2025-01-17 10:18:52 +0900

    Quartz sync: Jan 17, 2025, 10:18 AM

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 기록
 quartz 사용
+
+```bash
+npx quartz sync
+
+npx quartz sync --no-pull
+
+npx quartz build --serve
+```
+
+
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'changhyeon363/blog',
+        // from data-repo-id
+        repoId: 'R_kgDONlugaA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONlugaM4CmExl',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/changhyeon363",
